6633f3a23be4c8351c2bc829f52aab9af33515fd
Gatis Paeglis <gatis.paeglis@qt.io>
2016-06-07 09:55:50 +0200

    Update license information.
    Change-Id: I899c064958fcf70ada63d9145d8a2c6c43ee53d7
    Reviewed-by: Gatis Paeglis <gatis.paeglis@qt.io>

--- qt-ostree/ostree-remount.sh
+++ qt-ostree/ostree-remount.sh
@@ -1,27 +1,34 @@
 #!/bin/sh
 #############################################################################
 ##
-## Copyright (C) 2015 Digia Plc and/or its subsidiary(-ies).
+## Copyright (C) 2016 The Qt Company Ltd.
+## Contact: https://www.qt.io/licensing/
 ##
-## This file is part of the Qt Enterprise Embedded Scripts of the Qt
-## framework.
+## This file is part of the Qt OTA Update module of the Qt Toolkit.
 ##
-## $QT_BEGIN_LICENSE$
-## Commercial License Usage Only
-## Licensees holding valid commercial Qt license agreements with Digia
-## with an appropriate addendum covering the Qt Enterprise Embedded Scripts,
-## may use this file in accordance with the terms contained in said license
-## agreement.
+## $QT_BEGIN_LICENSE:GPL$
+## Commercial License Usage
+## Licensees holding valid commercial Qt licenses may use this file in
+## accordance with the commercial license agreement provided with the
+## Software or, alternatively, in accordance with the terms contained in
+## a written agreement between you and The Qt Company. For licensing terms
+## and conditions see https://www.qt.io/terms-conditions. For further
+## information use the contact form at https://www.qt.io/contact-us.
 ##
-## For further information use the contact form at
-## http://www.qt.io/contact-us.
-##
+## GNU General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU
+## General Public License version 3 or (at your option) any later version
+## approved by the KDE Free Qt Foundation. The licenses are as published by
+## the Free Software Foundation and appearing in the file LICENSE.GPL3
+## included in the packaging of this file. Please review the following
+## information to ensure the GNU General Public License requirements will
+## be met: https://www.gnu.org/licenses/gpl-3.0.html.
 ##
 ## $QT_END_LICENSE$
 ##
 #############################################################################
 
-# This script is used by system v init system in ostree based images.
+# This script is used by system V init system in ostree based images.
 case "$1" in
 start)
     /usr/sbin/ostree-remount
